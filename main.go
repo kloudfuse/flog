92a8956b1bf8c141cb308812dbf0c1700775b4a1
Jagan <jt@kloudfuse.com>
2022-06-03 11:42:52 -0700

    Add main cli runner
    Signed-off-by: Jagan <jt@kloudfuse.com>

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/mingrammer/cfmt"
+	"github.com/kloudfuse/flog/flog"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	opts := flog.ParseOptions()
+	if err := flog.Run(opts); err != nil {
+		cfmt.Warningln(err.Error())
+	}
+}
